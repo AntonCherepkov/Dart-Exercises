f81a06e6ccc00ba8d9cc256895c29cde523b00f4
Anton Cherepkov <phantomnbdg2@gmail.com>
2025-11-23 11:28:09 +0500

    trainig task: input data

--- startProject/console_prog/bin/console_input.dart
+++ startProject/console_prog/bin/console_input.dart
@@ -1,24 +1,38 @@
 import 'dart:io';
 
 void main(List<String> parameters) {
-  print('Please, input number: ');
+  print('Пожалуйста, введите номер: ');
   String? inputUser = stdin.readLineSync();
   var data = int.tryParse(inputUser!);
 
-  print('You are write: ${data}');
-  print('Type data: ${data.runtimeType}');
+  print('Вы ввели номер: ${data}');
+  print('Тип переменной: ${data.runtimeType}');
 
-  print('Please, input list numbers:');
+  print('Пожалуйста, введите последовательность чисел:');
   String? inputListUser = stdin.readLineSync();
-  
-  if (inputListUser == null || inputListUser.isEmpty) {
-    print('Data not faund');
-    return;
-
-  } else {
-
+  if (inputListUser != null || inputListUser!.isEmpty) {
     List<String> newDataString = inputListUser!.split(' ');
     List<int> newDataInt = newDataString.map(int.parse).toList();
-    print('We are iterable numbers: ${newDataInt}');
+    print('Вы ввели последовательность: ${newDataInt}');
   }
+
+  print('Пожалуйста, введите последовательность чисел:');
+  String? input = stdin.readLineSync();
+  if (input != null || input != '') {
+    Set<String> setInputStr = Set<String>.from(input!.split(' '));
+    Set<int> setInputInt = Set<int>.from(setInputStr.map(int.parse));
+    print('Вы ввели последовательность: ${setInputInt}');
+  }
+
+  print('Пожалуйста, введите ключи для мапки:');
+  String? keys = stdin.readLineSync();
+  List<String> keysList = [...keys!.split(' ')];
+
+  print('Пожалуйста, введите значения для мапки:');
+  String? values = stdin.readLineSync();
+  List<String> valuesListStr = List<String>.from(values!.split(' '));
+  
+  var valuesListInt = List<int>.from(valuesListStr.map(int.parse));
+  Map<String, int> resultMap = Map<String, int>.fromIterables(keysList, valuesListInt);
+  print('Итоговая структура: ${resultMap}');
 }
