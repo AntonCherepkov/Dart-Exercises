965e844607488b52394cdcee0cb23062244525a0
Anton Cherepkov <cherepkov.a@INFOSYSCO.ru>
2025-12-09 18:02:45 +0500

    training task: overload operators

---
+++ startProject/issue_class_project/issue_class_1/issue_class_2/lib/src/issue_class_2_base.dart
@@ -0,0 +1,15 @@
+class Verctor2D {
+  final double x;
+  final double y;
+
+  static int createdVerctors = 0;
+
+  Verctor2D(this.x, this.y) {
+    createdVerctors++;
+  }
+  
+  double operator+(Verctor2D other) {
+    // TODO: release
+  }
+
+}
