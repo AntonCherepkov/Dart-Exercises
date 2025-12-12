6306f5a3f6a379c0ff28d63415c4b89bce09a360
Anton Cherepkov <cherepkov.a@INFOSYSCO.ru>
2025-12-12 10:25:07 +0500

    training task: class inheritence

--- startProject/issue_class_project/issue_class_3/lib/src/issue_class_3_base.dart
+++ startProject/issue_class_project/issue_class_3/lib/src/issue_class_3_base.dart
@@ -1,17 +1,41 @@
 import 'package:uuid/uuid.dart';
 
+typedef Json = Map<String, dynamic>;
+
 class SmartDevice {
   late String _id;
   final String name;
   bool isOnline;
 
   String get id => _id;
-  set setID(String number) => this._id = number;
+  // set setID(String number) => this._id = number;
 
+  /** Базовый конструктор базового класса. 
+   * ID генерится при инициации экз-ра */
   SmartDevice(this.name, {this.isOnline = false}) {
     var uuid = Uuid().v4.toString();
     _id = uuid;
   }
   
-  // SmartDevice.defaultNamed({name}) : this(name, )  
+  /** Именной конструктор базового класса.
+   * Использует базовый конструктор для ген. ID */
+  SmartDevice.defaultNamed({name, isOnline = true}) 
+    : this(name, isOnline: isOnline);
+
+  /** Фабричный конструктор базового класса.
+   * Использует разные сценарии для создания экз. дочерних классов */
+  factory SmartDevice.create(Json json) {
+    for (var MapEntry(:key, :value) in json.entries) {
+      if (key == "type") {
+        switch (value) {
+          case ("light"):
+            return 
+        }
+      }
+    }
+  }
 }
+
+class SmartLight extends SmartDevice {}
+class SmartThermostat extends SmartDevice {}
+class SmartCamera extends SmartDevice {}
