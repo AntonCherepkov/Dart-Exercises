fed432e2fcd184392ae73865452fd41e0196f0dc
Anton Cherepkov <phantomnbdg2@gmail.com>
2025-12-13 01:59:54 +0500

    trainig task: class inheritance

--- startProject/issue_class_project/issue_class_3/example/issue_class_3_example.dart
+++ startProject/issue_class_project/issue_class_3/example/issue_class_3_example.dart
@@ -1,30 +1,6 @@
 import 'package:issue_class_3/issue_class_3.dart';
 
 void main() {
-
-  List<Map<String, dynamic>> listMaps = [
-    {"type": 'light'},
-    {"type": 'hard'},
-    {"type": 'bugaga'},
-    {"no_type": "bugaga"}
-  ];
-
-  for (var json in listMaps) {
-    for (var MapEntry(:key, :value) in json.entries) {
-      if (key == "type") {
-        switch(value) {
-          case ('light') :
-            print("this is light");
-            break;
-          case('hard') :
-            print("this is hard");
-            break;
-          case _ :
-            print("i don't know arguments");
-        }
-      } else {
-        throw ArgumentError("invalid key");
-      }
-    }
-  }
+  var device = SmartDevice.create(name: "bulb", json: {"type": "light"});
+  print('device_id: ${device.id}\nname: ${device.name}\nstatus: ${device.isOnline}');
 }
--- startProject/issue_class_project/issue_class_3/lib/src/issue_class_3_base.dart
+++ startProject/issue_class_project/issue_class_3/lib/src/issue_class_3_base.dart
@@ -2,40 +2,84 @@ import 'package:uuid/uuid.dart';
 
 typedef Json = Map<String, dynamic>;
 
+/// -----------------------------------------------------------------------------------------------
 class SmartDevice {
   late String _id;
   final String name;
-  bool isOnline;
+  bool? isOnline;
 
   String get id => _id;
-  // set setID(String number) => this._id = number;
 
-  /** Базовый конструктор базового класса. 
-   * ID генерится при инициации экз-ра */
-  SmartDevice(this.name, {this.isOnline = false}) {
-    var uuid = Uuid().v4.toString();
+  // Базовый конструктор базового класса. 
+  // ID генерится при инициации экз-ра 
+  // Если isOnline не указан, то делаем его false
+  SmartDevice(this.name, [this.isOnline]) {
+    var uuid = Uuid().v4();
     _id = uuid;
+    isOnline = isOnline ?? false;
   }
   
-  /** Именной конструктор базового класса.
-   * Использует базовый конструктор для ген. ID */
-  SmartDevice.defaultNamed({name, isOnline = true}) 
-    : this(name, isOnline: isOnline);
+  // Именной конструктор базового класса.
+  // Использует базовый конструктор для ген. ID */
+  SmartDevice.defaultNamed({
+    required name, 
+    required isOnline
+  }) : this(name, isOnline);
 
-  /** Фабричный конструктор базового класса.
-   * Использует разные сценарии для создания экз. дочерних классов */
-  factory SmartDevice.create(Json json) {
+  // Фабричный конструктор базового класса.
+  // Использует разные сценарии для создания экз. дочерних классов
+  factory SmartDevice.create({name, isOnline, json}) {
     for (var MapEntry(:key, :value) in json.entries) {
       if (key == "type") {
         switch (value) {
           case ("light"):
-            return 
+            return SmartLight.whiteDefault(name: name, isOnline: isOnline);
+          // case ("thermostat"):
+          //   return SmartThermostat();
+          // case ("cam") :
+          //   return SmartCamera();
+          case _:
+            throw ArgumentError("Не известный аргумент: ${value}");
         }
+      } else {
+        throw ArgumentError("Не известный аргумент: ${key}");
       }
     }
+    print('Не смогли создать дочернего класса по предоставленным данным!');
+    return SmartDevice(name, isOnline);
   }
 }
 
-class SmartLight extends SmartDevice {}
-class SmartThermostat extends SmartDevice {}
-class SmartCamera extends SmartDevice {}
+/// -----------------------------------------------------------------------------------------------
+class SmartLight extends SmartDevice {
+  late int _brightness;
+  late String _color;
+
+  int get brightness => _brightness;
+  String get colore => _color;
+  set brightness(int value) => _brightness = value;
+  set color(String value) => _color = value; 
+
+  SmartLight(
+    super.name, 
+    super.isOnline,
+    _brightenss, 
+    _colore, 
+    );
+
+  SmartLight.whiteDefault({
+    required super.name,
+    required super.isOnline,
+    brightness = 100, 
+    color = "white"
+  }): _brightness = brightness, 
+      _color = color,
+      super.defaultNamed();
+}
+
+/// -----------------------------------------------------------------------------------------------
+
+// class SmartThermostat extends SmartDevice {
+//   SmartThermostat(super.name);
+// }
+// class SmartCamera extends SmartDevice {}
