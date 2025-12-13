7d0cc6c89e82aac92d88b4f3d40d5fcbdac6619f
Anton Cherepkov <phantomnbdg2@gmail.com>
2025-12-13 17:40:35 +0500

    training task: class inheritance

--- startProject/issue_class_project/issue_class_3/example/issue_class_3_example.dart
+++ startProject/issue_class_project/issue_class_3/example/issue_class_3_example.dart
@@ -1,6 +1,12 @@
 import 'package:issue_class_3/issue_class_3.dart';
 
 void main() {
-  var device = SmartDevice.create(name: "bulb", json: {"type": "light"});
-  print('device_id: ${device.id}\nname: ${device.name}\nstatus: ${device.isOnline}');
+  DeviceGroup MyHome = DeviceGroup([
+    SmartDevice.create(name: "light_toilet", isOnline: true, json: {"type": "light"}),
+    SmartDevice.create(name: "liv_room_ligt", isOnline: true, json: {"type": "light"}),
+    SmartDevice.create(name: "street_therm", isOnline: true, json: {"type": "thermostat"}),
+    SmartDevice.create(name: "street_cam", isOnline: true, json: {"type": "cam"})
+  ]);
+
+  print(MyHome.toString());
 }
--- startProject/issue_class_project/issue_class_3/lib/src/issue_class_3_base.dart
+++ startProject/issue_class_project/issue_class_3/lib/src/issue_class_3_base.dart
@@ -2,7 +2,9 @@ import 'package:uuid/uuid.dart';
 
 typedef Json = Map<String, dynamic>;
 
-/// -----------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------
+// Базовый класс
+
 class SmartDevice {
   late String _id;
   final String name;
@@ -34,10 +36,12 @@ class SmartDevice {
         switch (value) {
           case ("light"):
             return SmartLight.whiteDefault(name: name, isOnline: isOnline);
-          // case ("thermostat"):
-          //   return SmartThermostat();
-          // case ("cam") :
-          //   return SmartCamera();
+          case ("thermostat"):
+            return SmartThermostat.defaultNamed(
+              name: name, isOnline: isOnline, temperature: 20.0, ecoMode: true);
+          case ("cam") :
+            return SmartCamera.named(
+              name: name, isOnline: isOnline, storageMb: 512, isRecording: true);
           case _:
             throw ArgumentError("Не известный аргумент: ${value}");
         }
@@ -48,9 +52,21 @@ class SmartDevice {
     print('Не смогли создать дочернего класса по предоставленным данным!');
     return SmartDevice(name, isOnline);
   }
+
+  void turnOn() => isOnline = true;
+  void turnOff() => isOnline = false;
+  void reboot() {
+    print("Перезагрузка: ");
+    turnOff();
+    print("Выключено");
+    turnOn();
+    print("Включено");
+  }
 }
 
-/// -----------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------
+// Дочерний класс Лампы
+
 class SmartLight extends SmartDevice {
   late int _brightness;
   late String _color;
@@ -75,11 +91,139 @@ class SmartLight extends SmartDevice {
   }): _brightness = brightness, 
       _color = color,
       super.defaultNamed();
+
+  @override
+  void reboot() {
+    brightness = brightness < 20 ? 20 : brightness;
+    super.reboot();
+  }
 }
 
-/// -----------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------
+// Дочерний класс Термостата 
 
-// class SmartThermostat extends SmartDevice {
-//   SmartThermostat(super.name);
-// }
-// class SmartCamera extends SmartDevice {}
+class SmartThermostat extends SmartDevice {
+  late double? temperature;
+  bool ecoMode;
+
+  SmartThermostat(
+    name,
+    isOnline,
+    this.temperature,
+    this.ecoMode
+  ): super(name, isOnline);
+
+  SmartThermostat.defaultNamed({
+    required super.name,
+    required super.isOnline,
+    temperature,
+    ecoMode
+  }): this.temperature = temperature,
+      this.ecoMode = ecoMode,
+      super.defaultNamed();
+
+  @override
+  void turnOn() {
+    ecoMode = false;
+    super.turnOn();
+  } 
+}
+
+// ------------------------------------------------------------------------------------------------
+class SmartCamera extends SmartDevice {
+  late bool isRecording;
+  late final int storageMb;
+  
+  SmartCamera(
+    super.name,
+    super.isOnline,
+    this.storageMb,
+    {isRecording = false}
+  );
+
+  SmartCamera.named({
+    name, isOnline, storageMb, isRecording
+  }): this(name, isOnline, storageMb, isRecording: false);
+
+  factory SmartCamera.fromStorage(name, isOnline, storageMb) {
+    if (storageMb < 511) {
+      return SmartMiniCamera.limitedStorage(
+        name: name, isOnline: isOnline, storageMb: storageMb);
+    } else {
+      return SmartCamera(name, isOnline, storageMb);
+    }
+  }
+
+  @override
+  void turnOff() {
+    isRecording = false;
+    super.turnOff();
+  }
+}
+
+// ------------------------------------------------------------------------------------------------
+// Дочерний от дочернего класса 
+
+class SmartMiniCamera extends SmartCamera {  
+
+  // ограничение по объему памяти для Mini камеры
+  static int storageLimit(storage) => storage = storage <= 256 ? storage : 256;
+
+  SmartMiniCamera.limitedStorage({
+    name, isOnline, storageMb
+  }): super.named(name: name, isOnline: isOnline, storageMb: storageLimit(storageMb));
+}
+
+// ------------------------------------------------------------------------------------------------
+
+typedef SmartHome = List<SmartDevice>;
+
+class DeviceGroup {
+  SmartHome devices = [];
+
+  SmartDevice operator[](int index) {
+    if (index < 0 || index > devices.length) {
+      throw IndexError.withLength(
+        index, devices.length, message: "Индекс за границами диапазона");
+    } 
+    return devices[index];
+  }
+
+  void add(SmartDevice device) => devices.add(device);
+  void addAll(List<SmartDevice> devices) => devices.addAll(devices);
+  void clear() => devices.clear();
+  Map asMap() => devices.asMap();
+  bool removeAt(int index) {
+    int len = devices.length;
+    devices = [
+      ...devices.sublist(0, index),
+      ...devices.sublist(index + 1)
+    ];
+    
+    return len != devices.length;
+  }
+
+  bool remove(String name) {
+    final int index_remove = devices.indexWhere((device) => device.name == name);
+    if (index_remove < 0) {
+      return false;
+    }
+    return removeAt(index_remove);
+  }
+
+  String toString() {
+    StringBuffer buffer = StringBuffer();
+    for (SmartDevice device in devices) {
+      buffer.write("ID: ${device.id}\tDevice: ${device.name}\tOnline: ${device.isOnline}\n");
+    }
+    return buffer.toString();
+  }
+
+  DeviceGroup(var add_devices){
+    if (add_devices is SmartDevice) {
+      devices.add(add_devices);
+    } else if (add_devices is List<SmartDevice>) {
+      devices.addAll(add_devices);
+    }
+  }
+}
