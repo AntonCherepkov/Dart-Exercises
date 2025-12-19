4038d2314858ec5e8a8df3fa75d3d0f5ef71a786
Anton Cherepkov <cherepkov.a@INFOSYSCO.ru>
2025-12-19 09:55:43 +0500

    trainig task: mixins

--- startProject/issue_class_project/issue_class_5/lib/src/usd.dart
+++ startProject/issue_class_project/issue_class_5/lib/src/usd.dart
@@ -1,23 +1,38 @@
 part of 'monay.dart';
 
-// class Usd extends Monay {
-//   late final int _cent;
+class Usd extends Monay {
+  late final int _cent;
 
+  Usd(value):super(value, "USD");
 
-//   void set cent(cent) => _cent = cent;
-//   int get cent => _cent;
+  void set cent(cent) => _cent = cent;
+  int get cent => _cent;
 
-//   @override
-//   Monay operator +(Object other) {
-//     // TODO: implement +
-//     throw UnimplementedError();
-//   }
+  @override
+  Monay operator +(Object other) {
+    if (other is Usd) { 
+      return Usd(other.value + value);
+    } else if (other is double){
+      return Usd(other + value);
+    }
+    throw UnimplementedError();
+  }
 
-//   @override
-//   Monay operator -(Object other) {
-//     // TODO: implement -
-//     throw UnimplementedError();
-//   }
+  @override
+  Monay operator -(Object other) {
+    double? result;
+    if (other is Usd) {
+      result = value - other.value;
+    } else if (other is double) {
+      result = value - other;
+    }
+    if (result != null && result < 0) {
+      throw ArgumentError("Банкрот");
+    } else {
+      return Usd(result);
+    }
+  }
 
-  
-// }
+  @override 
+  String toString() => "${name}: ${value.toInt()}\$ ${smallMonay}cents";
+}
