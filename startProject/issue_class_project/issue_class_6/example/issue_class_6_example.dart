d5d31bc3274c95f3692d7930017a0573e434550e
Anton Cherepkov <cherepkov.a@INFOSYSCO.ru>
2025-12-25 18:45:40 +0500

    training task: extention type

--- startProject/issue_class_project/issue_class_6/example/issue_class_6_example.dart
+++ startProject/issue_class_project/issue_class_6/example/issue_class_6_example.dart
@@ -1,9 +1,11 @@
 import 'package:issue_class_6/issue_class_6.dart';
 
 void main() {
-  String valueMask = NameString("23453").masked;
-  print(valueMask);
-  print(NameString("234qadf").isDigit);
-  print(NameString("dsafa23").isValid);
-  print(NameString("4645545").isValid);
+  // String valueMask = NameString("23453").masked;
+  // print(valueMask);
+  // print(NameString("234qadf").isDigit);
+  // print(NameString("dsafa23").isValid);
+  // print(NameString("4645545").isValid);
+  List a = [];
+
 }
--- startProject/issue_class_project/issue_class_6/lib/src/issue_class_6_base.dart
+++ startProject/issue_class_project/issue_class_6/lib/src/issue_class_6_base.dart
@@ -1,9 +1,12 @@
 extension type NameString(String value) {
   bool get isDigit => RegExp(r'^\d+$').hasMatch(value); 
+  
   bool get isValid => value.isNotEmpty && this.isDigit && value.length >= 5;
+  
   String get masked => value.replaceRange(
     (value.length ~/ 2) - (value.length ~/ 2 ~/ 2), 
     (value.length ~/ 2) + (value.length ~/ 2 ~/ 2), '*');
+  
   int toInt() {
     int? parseValue = int.tryParse(value);
     if (parseValue != null) {
@@ -11,5 +14,23 @@ extension type NameString(String value) {
     } else {
       throw TypeError();
     }
+  }
+}
+
+extension type Box<T>(List<T> _itmes) {
+  
+  void add (T elem) => _itmes.add(elem);
+  
+  T? firstOrNull() => _itmes.isEmpty ? null : _itmes[0];
+  
+  Iterable<T> where(bool Function(T) predicate) {
+    return List.from(_itmes.where(predicate));
+  }
+
+  Box<R> map<R>(R Function(T) mapper) {
+    return Box(
+            List.from(
+              _items.map(
+                (e) => mapper(e))));
   }
 }
