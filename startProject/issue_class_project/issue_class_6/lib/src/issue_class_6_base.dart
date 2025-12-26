63031dc62ed4a77174216b5b83d76bb457b94bcd
Anton Cherepkov <cherepkov.a@INFOSYSCO.ru>
2025-12-26 10:53:01 +0500

    trainig task: extension type

--- startProject/issue_class_project/issue_class_6/lib/src/issue_class_6_base.dart
+++ startProject/issue_class_project/issue_class_6/lib/src/issue_class_6_base.dart
@@ -1,3 +1,4 @@
+// Расширение над простой строкой, вмлидация и пару методов -------------------
 extension type NameString(String value) {
   bool get isDigit => RegExp(r'^\d+$').hasMatch(value); 
   
@@ -17,16 +18,19 @@ extension type NameString(String value) {
   }
 }
 
+// generic-расширение над списком типовых данных ------------------------------
 extension type Box<T>(List<T> _items) {
   
   void add (T elem) => _items.add(elem);
   
   T? firstOrNull() => _items.isEmpty ? null : _items[0];
   
+  // Вернет список, значения которого совпадут с условиями в perdicate - (callable) 
   Iterable<T> where(bool Function(T) predicate) {
     return List.from(_items.where(predicate));
   }
 
+  // Произвудет map действия с каждым элементом внутри расширяемого действия
   Box<R> map<R>(R Function(T) mapper) {
     return Box(
             List.from(
@@ -35,20 +39,33 @@ extension type Box<T>(List<T> _items) {
   }
 }
 
+// Пользовательский класс -----------------------------------------------------
 class UserDto {
-  final String id;
+  late final String id;
   final String name;
   final List<String> roles;
 
   UserDto(this.id, this.name, this.roles);
 }
 
+// Расширение над Id пользователя, добавим валидацию --------------------------
+extension type UserId(String id) {
+  bool validId() {
+    String pattern = r'^[A-Z0-9]{5}$';
+    RegExp regexp = RegExp(pattern);
+    return regexp.hasMatch(id);
+  }
+}
+
+// Расшерение над пользовательским классом с информацией о пользователе -------
 extension type User(UserDto dto) {
-  // TODO: реализовать
+  UserId get userId => UserId(dto.id);
+  bool get isAdmin => dto.roles.contains('admin');
+  String get displayName => "Name ${dto.id}";
 }
 
 extension type UserResponse(Map<String, dynamic> raw) {
   Iterable<User> parseUsers() sync* {
     // TODO: реализовать
-  }
+   }
 }
